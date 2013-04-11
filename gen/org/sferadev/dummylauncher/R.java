c58992d75b516a7b525310130b6d6548604e7f06
SferaDev <sferarc@gmail.com>
2013-04-11 12:04:10 +0200

    Pre Release Files

--- gen/org/sferadev/dummylauncher/R.java
+++ gen/org/sferadev/dummylauncher/R.java
@@ -55,17 +55,20 @@ public final class R {
         public static final int textView3=0x7f080007;
     }
     public static final class layout {
-        public static final int activity_launcher=0x7f030000;
-        public static final int applauncher=0x7f030001;
-        public static final int applauncherrow=0x7f030002;
+        public static final int activity_about=0x7f030000;
+        public static final int activity_launcher=0x7f030001;
+        public static final int applauncher=0x7f030002;
+        public static final int applauncherrow=0x7f030003;
     }
     public static final class menu {
-        public static final int launcher=0x7f070000;
+        public static final int about=0x7f070000;
+        public static final int launcher=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_about=0x7f050003;
     }
     public static final class style {
         /** 
