f6d6677c5e74e3f9998613e4ef36a412db861a13
SferaDev <sferarc@gmail.com>
2013-05-03 14:31:21 +0200

    Dutch Translation

--- gen/org/sferadev/dummylauncher/R.java
+++ gen/org/sferadev/dummylauncher/R.java
@@ -93,6 +93,10 @@ public final class R {
         public static final int launcher=0x7f080000;
     }
     public static final class string {
+        public static final int Instellingen_defaults_summary=0x7f060019;
+        public static final int Instellingen_defaults_title=0x7f060018;
+        public static final int Instellingen_title=0x7f060017;
+        public static final int action_Instellingen=0x7f060016;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int applauncher_all=0x7f06000c;
